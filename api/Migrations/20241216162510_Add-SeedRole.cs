1806f973ab8db6dd06a2e7b06d394601787a5862
Michael Sogunro <m.sogunro@lagetronix.com>
2024-12-16 17:30:56 +0100

    adding user registration

---
+++ api/Migrations/20241216162510_Add-SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "38288b82-6f05-4af7-b0bd-8246912bdda9", null, "Admin", "ADMIN" },
+                    { "98c6424d-e7cd-4532-95db-60858b8d47c0", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "38288b82-6f05-4af7-b0bd-8246912bdda9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "98c6424d-e7cd-4532-95db-60858b8d47c0");
+        }
+    }
+}
